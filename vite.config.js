1d2f24e447124ab5515929345aeab91afaa18ab1
YURII SILIN <try2scumme@gmail.com>
2023-03-18 18:07:37 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vite-plug',
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
+  plugins: [react({
+    exclude: /\.stories\.(t|j)sx?$/,
+    include: '**/*.tsx',
+  }),
+  ],
+
+  base: '/',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
+  define: {
+    'process.env': process.env
+  }
 })
+
